e76fe86ff106db0a42838941aecdb184bf62e8fb
romeWarrier <33370853+romeWarrier@users.noreply.github.com>
2018-01-28 21:58:32 +0700

    add map

--- screens/NearbyScreen.js
+++ screens/NearbyScreen.js
@@ -11,14 +11,45 @@ import {
 
 import { MonoText } from '../components/StyledText';
 
-export default class SearchScreen extends React.Component {
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 
-
+export default class NearbyScreen extends React.Component {
+  constructor(props){
+    super(props)
+    //this.onRegionChange = this.onRegionChange.bind(this)
+    this.state = {
+      region: {
+        latitude: 13.819112,
+        longitude: 100.514352,
+        latitudeDelta: 0.015,
+        longitudeDelta: 0.0121,
+      },
+    }
+  
+  }
+  /*onRegionChange = (region) => {
+    console.warn('onRegionChange')
+    this.setState({ region });
+  }*/
   render() {
-    
+    //const { region } = this.props;
+    //console.log(region);
     return (
       <View style={styles.container}>
-        <Text>Nearby</Text>
+        <MapView
+          style={styles.map}
+          region={this.state.region}
+          //onRegionChange={region=>{this.setState({region});}}
+          /*region={this.state.region}
+          onRegionChange={this.onRegionChange}
+          region={{
+            latitude: 13.819112,
+            longitude: 100.514352,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}*/
+        />
       </View>
     );
   }
@@ -27,7 +58,13 @@ export default class SearchScreen extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    height: 500,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
   },
 });
