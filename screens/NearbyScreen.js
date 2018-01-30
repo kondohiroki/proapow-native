df56ce31e504503573d8d6b263d4937fb5995fc8
ThunderLightning <thunderman369@hotmail.com>
2018-01-30 23:20:29 +0700

    แก้ไข map code

--- screens/NearbyScreen.js
+++ screens/NearbyScreen.js
@@ -7,6 +7,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions,
 } from 'react-native';
 
 import { MonoText } from '../components/StyledText';
@@ -16,19 +17,28 @@ import { Marker } from 'react-native-maps';
 import { empty } from 'apollo-client/node_modules/apollo-link';
 //import { error } from 'util';
 
+const {width, height} = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width/height
+const LATTITUDE_DELTA = 0.0922
+const LONGTITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
+
 export default class NearbyScreen extends React.Component {
   constructor(props){
     super(props)
+
     this.state = {
       initialPosition: {
-        latitude: 13.819112,
-        longitude: 100.514352,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
       },
       markerPosition: {
-        latitude: 13.819112,
-        longitude: 100.514352
+        latitude: 0,
+        longitude: 0
       },
       markers: [{
         title: 'KMUTNB rocket',
@@ -74,11 +84,14 @@ export default class NearbyScreen extends React.Component {
     };
 
     navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords
-        const initialRegion = {
-          //...this.state.region,
-          latitude,
-          longitude
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
+
+        var initialRegion = {
+          latitude: lat,
+          longitude: long,
+          latitudeDelta: LATTITUDE_DELTA,
+          longitudeDelta: LONGTITUDE_DELTA
         }
         /*onMapReady=() => {
           this.setState({ regionSet: true });
@@ -87,17 +100,23 @@ export default class NearbyScreen extends React.Component {
         this.setState({markerPosition: initialRegion})
       },
       (error) => alert(JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+      {enableHighAccuracy: true, timeout: 25000, maximumAge: 3600000})
 
       this.watchID = navigator.geolocation.watchPosition((position) => {
-        const { latitude, longitude } = position.coords
-        const lastRegion = {
-          latitude,
-          longitude
+        var lat = parseFloat(position.coords.latitude)
+        var long = parseFloat(position.coords.longitude)
+
+        var lastRegion = {
+          latitude: lat,
+          longitude: long,
+          longitudeDelta: LONGTITUDE_DELTA,
+          latitudeDelta: LATTITUDE_DELTA
         }
         this.setState({initialPosition: lastRegion})
         this.setState({markerPosition: lastRegion})
-      },error,{})
+      },
+      (error) => alert(JSON.stringify(error)),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 2000, distanceFilter: 10})
   }
 
   componentWillUnmount() {
@@ -116,25 +135,23 @@ export default class NearbyScreen extends React.Component {
       <View style={styles.container}>
         <MapView
           style={styles.map}
-          initialRegion={this.state.initialPosition}
+          region={this.state.initialPosition}
           //onRegionChange={this.onRegionChange}
         >
-        {this.state.markers.map(marker => (
-          <MapView.Marker 
-            coordinate={marker.coordinates}
-            title={marker.title}
-            image={require('../assets/images/smallpin.png')}
-          />
-        ))}
-        {this.state.markers.map(marker => (
-          <MapView.Marker 
-            coordinate={this.state.markerPosition}>
-            <View style={styles.radius}>
-              <View style={styles.trackMarker}>
-              </View>
-            </View>
-          </MapView.Marker>
-        ))}
+          {this.state.markers.map(marker => (
+            <MapView.Marker 
+              coordinate={marker.coordinates}
+              title={marker.title}
+              image={require('../assets/images/smallpin.png')}
+            />
+          ))}
+          {this.state.markers.map(marker => (
+            <MapView.Marker 
+              coordinate={this.state.markerPosition}>
+                <View style={styles.trackMarker}>
+                </View>
+            </MapView.Marker>
+          ))}
         </MapView>
       </View>
     );
@@ -154,15 +171,16 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
   radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50/2,
+    height: 100,
+    width: 100,
+    borderRadius: 100/2,
     overflow: 'hidden',
     backgroundColor: 'rgba(0, 122, 255, 0.1)',
     borderWidth: 1,
     borderColor: 'rgba(0, 112, 255, 0.3)',
     alignItems: 'center',
     justifyContent: 'center',
+    //marginBottom: -25,
   },
   trackMarker: {
     height: 20,
@@ -171,7 +189,6 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     borderRadius: 20/2,
     overflow: 'hidden',
-    backgroundColor: '#007AFF',
-    
-  }
+    backgroundColor: '#37FF10',
+  },
 });
